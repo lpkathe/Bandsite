518797175239029c8d339ad156f55a12499cfa87
David Burgos <david.burgos@wundermanthompson.com>
2021-06-06 17:35:58 -0500

    initial commit.

--- sprint-three/scripts/index-page.js
+++ sprint-three/scripts/index-page.js
@@ -1,81 +1,24 @@
-const formButton = document.querySelector(".form__button");
-
-function captureValues() {
-  event.preventDefault();
-
-  const commentContainer = document.querySelector(".comment__container");
-  const firstChildContainer = commentContainer.firstChild;
-  const userPicture = document.querySelector(".user__picture").src;
-  const formName = document.querySelector(".form__name").value;
-  const formComment = document.querySelector(".form__comment").value;
-
-  const commentBox = document.createElement("div");
-  const userForm = document.createElement("div");
-  const userInfo = document.createElement("div");
-  const userName = document.createElement("h3");
-  const userDate = document.createElement("p");
-  const comment = document.createElement("p");
-  const commentLine = document.createElement("hr");
-
-  userName.innerText = formName;
-  const date = new Date();
-  userDate.innerText = date.toLocaleDateString();
-  comment.innerText = formComment;
-
-  if (userPicture != "") {
-    const userImg = document.createElement("img");
-    userImg.setAttribute("src", userPicture);
-    userImg.className = "user__picture";
-    commentBox.appendChild(userImg);
-  } else {
-    const userImg = document.createElement("p");
-    userImg.className = "user__picture";
-    commentBox.appendChild(userImg);
-  }
-
-  commentBox.className = "comment__box";
-  userForm.className = "user__form";
-  userInfo.className = "user__info";
-  userName.className = "user__name";
-  userDate.className = "user__date";
-  comment.className = "user__comment";
-  commentLine.className = "line";
-
-  commentContainer.insertBefore(commentBox, firstChildContainer);
-  commentBox.appendChild(userForm);
-  userForm.appendChild(userInfo);
-  userInfo.appendChild(userName);
-  userInfo.appendChild(userDate);
-  userForm.appendChild(comment);
-  commentContainer.appendChild(commentLine);
-
-  const form = document.querySelector(".form");
-  form.reset();
-}
-//add an event listener to the form submission
-formButton.addEventListener("click", captureValues);
-
-
-
 //Sprint 3
 
-console.log ("Hello you");
+console.log("Still alive");
 const apiUrl = 'https://project-1-api.herokuapp.com';
 
 const newcomments = document.getElementById('commentList');
 const commentForm = document.getElementById('form');
 
+//add an event listener to the form submission
+commentForm.addEventListener("submit", handleSubmit);
+
 //function to get apikey
 const apiKey = () => {
-axios
-.get(`${apiUrl}/register`)
-.then((response)=> {
-  const apiKey =  response.data.api_key
-  console.log(apiKey);
-})
-  .catch((error) => console.log('error getting apikey'));
+  axios
+    .get(`${apiUrl}/register`)
+    .then((response) => {
+      const apiKey = response.data.api_key
+      console.log(apiKey);
+    })
+    .catch((error) => console.log('error getting apikey'));
 };
-
 
 //function to get comments
 const getComments = () => {
@@ -83,29 +26,143 @@ const getComments = () => {
     .get(`${apiUrl}/comments?api_key=${apiKey}`)
     .then((response) => {
       const commentsArray = response.data;
-
-      console.log(response);
-
-      commentsArray.forEach((record) => {
-        // create new elements
-        const listEl = document.createElement('li');
-        const userCommentEl = document.createElement('p');
-        const userNameEl = document.createElement('h3');
-        const userLikeEl = document.createElement('p');
-
-        //attach the data to the elements
-        userNameEl.innerText = record.name;
-        userCommentEl.innerText = record.comment;
-        userLikeEl.innerText = record.likes;
-        //append the elements to their respective parents
-        listEl.appendChild(userNameEl);
-        listEl.appendChild(userCommentEl);
-        listEl.appendChild(userLikeEl);
-      
-        commentList.appendChild(listEl);
+      commentsArray.forEach((comment) => {
+        addComment(comment);
       });
     })
-    .catch((error) => console.log('error getting data comments'));
+    .catch((error) => console.log('error getting data comments:' + error));
 };
 
+//function to create each comment
+function addComment(comment) {
+  // create new elements
+  const container = document.createElement('div');
+  container.className = "section4__comment__box";
+  container.id = comment.id;
+  const image = document.createElement('p');
+  image.className = "section4__user__picture section4__user__picture--2";
+
+  const userForm = document.createElement('div');
+  userForm.className = "section4__user__form";
+
+  const userInfo = document.createElement('div');
+  userInfo.className = "section4__user__info";
+
+  const userName = document.createElement('h3');
+  userName.className = "section4__user__name";
+  userName.innerText = comment.name;
+
+  const date = new Date(comment.timestamp);
+  const userDates = document.createElement('p');
+  userDates.className = "section4__user__date";
+  userDates.innerText = date.toLocaleDateString();
+
+  userInfo.appendChild(userName);
+  userInfo.appendChild(userDates);
+  userForm.appendChild(userInfo);
+
+  const userComment = document.createElement('p');
+  userComment.className = "section4__user__comment";
+  userComment.innerText = comment.comment;
+
+  userForm.appendChild(userComment);
+
+  container.appendChild(image);
+  container.appendChild(userForm);
+
+  const commentWrapper = document.createElement('div');
+  commentWrapper.className = "section4__comment__sub-container";
+
+  const commentLike = document.createElement('img');
+  commentLike.className = "section4__user__likes";
+  commentLike.setAttribute("src", "../assets/Icons/thumb-up.png");
+
+  const likeCounter = document.createElement("p");
+  likeCounter.className = "section4__user__likes--counter";
+  likeCounter.innerText = `${comment.likes} likes`;
+
+  const commentDelete = document.createElement('button');
+  commentDelete.innerText = "Remove";
+  commentDelete.className = "section4__button--remove";
+  commentDelete.addEventListener("click", (event) => {
+    removeComment(event.target.parentNode.id);
+  });
+
+  commentLike.addEventListener("click", (event) => {
+    likeComment(event.target.parentNode.id);
+  });
+
+  const divider = document.createElement('hr');
+  divider.className = "line";
+  commentWrapper.appendChild(container);
+  commentWrapper.appendChild(commentDelete);
+  commentWrapper.appendChild(likeCounter);
+  commentWrapper.appendChild(commentLike);
+  commentWrapper.appendChild(divider);
+
+  newcomments.insertBefore(commentWrapper, newcomments.firstChild);
+}
+
+//submit the form and add the comment to the list
+function handleSubmit(event) {
+  event.preventDefault();
+  const user = event.target.user.value;
+  const commentText = event.target.commentText.value;
+
+  const newComment = {
+    name: user, // => name: user
+    comment: commentText // => commentText: commentText
+  };
+  axios
+    .post(`${apiUrl}/comments?api_key=${apiKey}`, newComment, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
+    )
+    .then((response) => {
+      console.log('HTTP STATUS CODE: ', response.status);
+
+      //after comment has posted, getComments again.
+      getComments();
+    })
+    .catch((error) => console.log(error));
+
+  //reset the form
+  commentForm.reset();
+}
+
+//Like implementation for comments.
+function likeComment(id) {
+  if (id !== undefined) {
+    axios
+      .put(`${apiUrl}/comments/${id}/like?api_key=${apiKey}`)
+      .then((response) => {
+        if (response.status === 200) {
+          document.getElementById(id).querySelector(".section4__user__likes").innerText = response.data.likes + " likes";
+        } else {
+          console.log(`Error trying to like commentId: ${id} status:${response.status} - ${response}`);
+        }
+      })
+      .catch((error) => console.log(`error for like comment ${id}:` + error));
+  }
+}
+
+//Remove comment.
+function removeComment(id) {
+  if (id !== undefined) {
+    axios
+      .delete(`${apiUrl}/comments/${id}?api_key=${apiKey}`)
+      .then((response) => {
+        if (response.status === 200) {
+          document.getElementById(id).remove();
+        } else {
+          console.log(`Error removing commentId: ${id} status:${response.status} - ${response}`);
+        }
+      })
+      .catch((error) => console.log(`error for like comment ${id}:` + error));
+  }
+}
+
+//initial call to get comments  when first loaded.
 getComments();
